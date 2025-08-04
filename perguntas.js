ed82448fe9bfb01c683555a3e47d32a36711d4e3
6debora6 <debora.stumm7@gmail.com>
2025-08-04 11:52:57 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -21,8 +21,27 @@ criaCartao(
 )
 
 
-
-
+<div> class="cartao__conteudo__resposta">
+  <p>O que é JavaScript?</p>
+</div>
+    <><div> class="cartao__conteudo__resposta">
+        <p>O JavaScript é uma linguagem de programação</p>
+    </div><div> class="cartao__conteudo__resposta">
+            <p>O que é CSS?</p>
+        </div><div class="cartao__conteudo__resposta">
+            <p>O CSS é uma linguagem de estilização</p>
+        </div> 
+        <article class="cartao">
+            <div class="cartao__conteudo">
+                <h3>Geografia</h3>
+                <div class="cartao__conteudo__pergunta">
+                    <p>Qual a capital da França?</p>
+                </div>
+                <div class="cartao__conteudo__resposta">
+                    <p>A capital da França é Paris</p>
+                </div>
+            </div>
+        </article></>
 
 
 
